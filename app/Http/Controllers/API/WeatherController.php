3484319a02c601f19393542414b2c6e4c268d0c0
nipe <nikitarabinin20381@gmail.com>
2025-05-30 22:54:23 +0300

    fix bug with access to api

--- app/Http/Controllers/API/WeatherController.php
+++ app/Http/Controllers/API/WeatherController.php
@@ -5,21 +5,19 @@ namespace App\Http\Controllers\API;
 use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\Controller;
 use App\Models\City;
-use Illuminate\Http\Request;
 
 
 
 class WeatherController extends Controller
 {
     public function getWeatherTest(City $selectedCity) {
-        dd($selectedCity);
         return file_get_contents(base_path('tests/data/response.json'));
     }
 
     public function getWeather(City $selectedCity) {
         $response = Http::get("https://api.openweathermap.org/data/3.0/onecall?lat={$selectedCity->lat}&lon={$selectedCity->lon}&exclude=minutely,hourly,daily,alerts&appid={env(API_KEY)}");
 
-        if($response->seccessful()){
+        if($response->successful()){
             $data = $response->json();
             return $data;
         }
@@ -31,8 +29,8 @@ class WeatherController extends Controller
     public function Weather(string $city) {
         $selectedCity = City::select('lat', 'lon')->firstWhere('name', $city);
         if($selectedCity){
-            // return WeatherController::getWeather($selectedCity);
-            return WeatherController::getWeatherTest($selectedCity);
+            return WeatherController::getWeather($selectedCity);
+            // return WeatherController::getWeatherTest($selectedCity);
         }
         else{
             return response()->json(['error'=>'City not found'], 404);
