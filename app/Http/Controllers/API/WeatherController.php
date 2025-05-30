bb39eaf130f864bcc6a0c10079d13f177ca26799
nipe <nikitarabinin20381@gmail.com>
2025-05-30 22:40:12 +0300

    Add api to get weather

---
+++ app/Http/Controllers/API/WeatherController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\Controller;
+use App\Models\City;
+use Illuminate\Http\Request;
+
+
+
+class WeatherController extends Controller
+{
+    public function getWeatherTest(City $selectedCity) {
+        dd($selectedCity);
+        return file_get_contents(base_path('tests/data/response.json'));
+    }
+
+    public function getWeather(City $selectedCity) {
+        $response = Http::get("https://api.openweathermap.org/data/3.0/onecall?lat={$selectedCity->lat}&lon={$selectedCity->lon}&exclude=minutely,hourly,daily,alerts&appid={env(API_KEY)}");
+
+        if($response->seccessful()){
+            $data = $response->json();
+            return $data;
+        }
+        else{
+            return response()->json(['error' => 'API request failed'], $response->status());
+        }
+    }
+
+    public function Weather(string $city) {
+        $selectedCity = City::select('lat', 'lon')->firstWhere('name', $city);
+        if($selectedCity){
+            // return WeatherController::getWeather($selectedCity);
+            return WeatherController::getWeatherTest($selectedCity);
+        }
+        else{
+            return response()->json(['error'=>'City not found'], 404);
+        }
+    }
+}
---
+++ database/migrations/2025_05_30_175658_update_cities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->dropColumn('coordinates');
+            $table->double('lat');
+            $table->double('lon');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->point('coordinates');
+            $table->dropColumn('lat');
+            $table->dropColumn('lon');
+        });
+    }
+};
